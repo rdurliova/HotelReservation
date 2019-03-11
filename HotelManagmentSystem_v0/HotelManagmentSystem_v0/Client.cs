199cb312f6b6664ef69a39ff01db8cd8afff8211
rdurliova <reshide_durliova@abv.bg>
2019-03-11 12:25:57 +0200

    Create project and add HotelsReservationContext.cs

---
+++ HotelManagmentSystem_v0/HotelManagmentSystem_v0/Client.cs
@@ -0,0 +1,43 @@
+namespace HotelManagmentSystem_v0
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Client
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Client()
+        {
+            Reservations = new HashSet<Reservation>();
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(10)]
+        public string EGN { get; set; }
+
+        public int? Age { get; set; }
+
+        public int? TownID { get; set; }
+
+        [StringLength(30)]
+        public string Gsm { get; set; }
+
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        public virtual Town Town { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ HotelManagmentSystem_v0/HotelManagmentSystem_v0/Country.cs
@@ -0,0 +1,25 @@
+namespace HotelManagmentSystem_v0
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Country
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Country()
+        {
+            Towns = new HashSet<Town>();
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Town> Towns { get; set; }
+    }
+}
