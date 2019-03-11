2a11ecab77db13768008bf373d76324dc2cf2737
rdurliova <reshide_durliova@abv.bg>
2019-03-11 12:30:22 +0200

    Add folder Views and create classes

---
+++ HotelManagmentSystem_v0/HotelManagmentSystem_v0/Views/InOutConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHotelsReservation.Views
+{
+    public class InOutConsole
+    {
+        public string ReadCoutryName()
+        {
+            Console.Write("Въведи име на държава: ");
+            return Console.ReadLine();
+        }
+        public string ReadTownName()
+        {
+            Console.Write("Въведи име на град: ");
+            return Console.ReadLine();
+        }
+
+        public string ReadRoomType()
+        {
+            Console.Write("Въведи тип на стая: ");
+            return Console.ReadLine();
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ HotelManagmentSystem_v0/HotelManagmentSystem_v0/Views/MessageApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHotelsReservation.Views
+{
+    public class MessageApp
+    {
+        public const string EnterCorrectValues = "Въведи коректни стойности!";
+        public string MessageInsertCoutryTrue(string countryName)
+        {
+            string message = $"{countryName} е добавена успешно в таблцита с държави!";
+            return message;
+        }
+
+        public string MessageInsertCoutryFalse(string countryName)
+        {
+            string message = $"{countryName} вече съществува в таблцита с държави!";
+            return message;
+        }
+
+        public string MessageInsertTownTrue(string countryName, string townName)
+        {
+            string message = $"В държавата '{countryName}' е добавен нов град с име '{townName}'";
+            return message;
+        }
+
+        public string MessageInsertTownFalse(string countryName,string townName)
+        {
+            string message = $"В '{countryName}' вече съществува град с име '{townName}'!";
+            return message;
+        }
+
+
+        public string MessageInsertRoomTrue(string roomType)
+        {
+            string message = $"Добавена е нова стая от тип '{roomType}'";
+            return message;
+        }
+
+        public string MessageInsertRoomFalse(string roomType)
+        {
+            string message = $"{roomType} - не съществува!";
+            return message;
+        }
+
+    }
+}
