f0ff2d9fe120e1563a4d915e47f80042f42f65dc
rdurliova <reshide_durliova@abv.bg>
2019-03-23 10:18:33 +0200

    Create user form FormSystemOfHotelsReservation.Designer.cs

---
+++ Project/Project/Forms/FormSystemOfHotelsReservation.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Project.Forms
+{
+    partial class FormSystemOfHotelsReservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(41, 76);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(463, 199);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Система за хотелски резервации";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Резервация на стая";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(576, 76);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(409, 199);
+            this.panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(363, 42);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Освобождаване на стая";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Location = new System.Drawing.Point(41, 305);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(943, 185);
+            this.panel3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(259, 36);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Въвеждане на данни";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(332, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(259, 36);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Справки";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(638, 18);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(259, 36);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Отчети";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // FormSystemOfHotelsReservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 750);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "FormSystemOfHotelsReservation";
+            this.Text = "FormSystemOfHotelsReservation";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
